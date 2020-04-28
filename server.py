81d1fc2635e894297884e63d3281e727ebdbaebe
Pirmin Kalberer <pka@sourcepole.ch>
2020-04-28 22:12:13 +0200

    Add tenant config handling

--- server.py
+++ server.py
@@ -9,14 +9,16 @@ import requests
 from qwc_services_core.api import CaseInsensitiveArgument
 from qwc_services_core.app import app_nocache
 from qwc_services_core.auth import auth_manager, optional_auth, get_auth_user
-from qwc_services_core.permission import PermissionClient
+from qwc_services_core.tenant_handler import TenantHandler
+from qwc_services_core.runtime_config import RuntimeConfig
+from qwc_services_core.permissions_reader import PermissionsReader
 
 
 # Flask application
 app = Flask(__name__)
 app_nocache(app)
 api = Api(app, version='1.0', title='Document service API',
-          description="""API for SO!MAP Document service.
+          description="""API for QWC Document service.
 
 The document service delivers reports from the Jasper reporting service.
           """,
@@ -28,32 +30,36 @@ app.config['ERROR_404_HELP'] = False
 
 auth = auth_manager(app, api)
 
-JASPER_SERVICE_URL = os.environ.get('JASPER_SERVICE_URL',
-                                    'http://localhost:8002/reports')
-try:
-    JASPER_TIMEOUT = int(os.environ.get('JASPER_TIMEOUT', 60))
-except:
-    JASPER_TIMEOUT = 60
-
-permission = PermissionClient()
+tenant_handler = TenantHandler(app.logger)
+config_handler = RuntimeConfig("document", app.logger)
 
 
-def get_document(template, format):
+def get_document(tenant, template, format):
     """Return report with specified template and format.
 
     :param str template: Template ID
     :param str format: Document format
     """
-    permissions = permission.document_permissions(
-        template, get_auth_user()
+    config = config_handler.tenant_config(tenant)
+    jasper_service_url = config.get(
+        'jasper_service_url', 'http://localhost:8002/reports')
+    jasper_timeout = config.get("jasper_timeout", 60)
+
+    resources = config.resources().get('document_templates', [])
+    permissions_handler = PermissionsReader(tenant, app.logger)
+    permitted_resources = permissions_handler.resource_permissions(
+        'document_templates', get_auth_user()
     )
-    app.logger.info(permissions)
-    if permissions:
-        jasper_template = os.path.splitext(
-            permissions['report_filename']
-        )[0]
+    if template in permitted_resources:
+        resource = list(filter(
+            lambda entry: entry.get("template") == template, resources))
+        if len(resource) != 1:
+            app.logger.info("Template '%s' not found in config", template)
+            abort(404)
+        jasper_template = resource[0]['report_filename']
+
         # http://localhost:8002/reports/BelasteteStandorte/?format=pdf&p1=v1&..
-        url = "%s/%s/" % (JASPER_SERVICE_URL, jasper_template)
+        url = "%s/%s/" % (jasper_service_url, jasper_template)
         params = {"format": format}
         for k, v in request.args.lists():
             params[k] = v
@@ -61,13 +67,14 @@ def get_document(template, format):
         app.logger.info("Forward request to %s?%s" %
                         (url, urlencode(params)))
 
-        response = requests.get(url, params=params, timeout=JASPER_TIMEOUT)
+        response = requests.get(url, params=params, timeout=jasper_timeout)
         r = Response(
             stream_with_context(response.iter_content(chunk_size=16*1024)),
             content_type=response.headers['content-type'],
             status=response.status_code)
         return r
     else:
+        app.logger.info("Missing permissions for template '%s'", template)
         abort(404)
 
 
@@ -86,13 +93,14 @@ class Document(Resource):
 
         Query parameters are passed to the reporting engine.
         """
+        tenant = tenant_handler.tenant()
         pos = template.rfind('.')
         if pos != -1:
             format = template[pos + 1:]
             template = template[:pos]
         else:
             format = 'pdf'
-        return get_document(template, format)
+        return get_document(tenant, template, format)
 
 
 """ readyness probe endpoint """
