2ee34ad4296e5c160d4908e21432fb964bf8c234
Sandro Mani <manisandro@gmail.com>
2026-09-17 14:10:24 +0200

    Remove debug leftover (#36)

--- src/server.py
+++ src/server.py
@@ -27,7 +27,6 @@ class PrefixMiddleware:
         self.prefix = prefix.rstrip("/")
 
     def __call__(self, environ, start_response):
-        print(environ)
         path = environ.get("PATH_INFO", "")
         script_name = environ.get("SCRIPT_NAME", "")
 
